f9328a2ceaf346bcbb33038f078c1b7b8cd891c7
ssmichaelm <michael.moreland@smoothstack.com>
2022-03-08 14:56:38 -0800

    Updated parameters to obfuscate values

--- microservices/deploy.sh
+++ microservices/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-kubectl apply -f user.yml
-kubectl apply -f bank.yml
-kubectl apply -f transaction.yml
-kubectl apply -f underwriter.yml
+# Deploy K8s deployments and services
+# kubectl apply -f user.yml
+# kubectl apply -f bank.yml
+# kubectl apply -f transaction.yml
+# kubectl apply -f underwriter.yml
+
+# Destroy
+# kubectl delete deployment user-deployment
+# kubectl delete deployment bank-deployment
+# kubectl delete deployment transaction-deployment
+# kubectl delete deployment underwriter-deployment
